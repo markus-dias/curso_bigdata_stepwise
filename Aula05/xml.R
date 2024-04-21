d20b92d68c64a34c21bbaee5632bf598e5d6c1b4
Markus Dias <mdias@stepwise.com.br>
2024-04-21 10:45:39 -0300

    aula 04-15

--- Aula05/xml.R
+++ Aula05/xml.R
@@ -1,11 +1,14 @@
 if (!require("xml2")) {
   install.packages("xml2")
+  require("xml2")
 }
 if (!require("XML")) {
   install.packages("XML")
+  require("XML")
 }
 if (!require("here")) {
   install.packages("here")
+  require("here")
 }
 library("tibble")
 
@@ -14,7 +17,7 @@ print(caminho_arquivos)
 
 dados_carros_eletricos <- read_xml(caminho_arquivos)
 
-xml_structure(dados_carros_eletricos)
+# xml_structure(dados_carros_eletricos)
 
 cep <- xml_text(xml_find_all(dados_carros_eletricos, ".//zip_code"))
 modelo <- xml_text(xml_find_all(dados_carros_eletricos, ".//model"))
@@ -25,5 +28,7 @@ dataframe_cep_modelos <- tibble(
   modelo = modelo[1:100000],
   ano_modelo = ano_modelo[1:100000]
 )
+
+# view(dataframe_cep_modelos)
 
 print(dataframe_cep_modelos)
